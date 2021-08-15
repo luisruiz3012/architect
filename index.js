ef8b9fc14a32f984678f91a922989be4309dcab6
luisruiz3012 <luisdiegoaguilar@outlook.com>
2021-08-14 22:22:41 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.main-banner', {
+    duration: 2000,
+});
+
+sr.reveal('.projects', {
+    duration: 1500,
+});
+
+sr.reveal('.services', {
+    duration: 1500,
+});
+
+sr.reveal('.gallery', {
+    duration: 2000,
+});
